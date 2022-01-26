57d58ca0823aae0c2e087df64cfc553862b99448
Guoqiang QI <guoqiang.qi1@gmail.com>
2022-01-26 10:16:28 +0000

    Add messages for debug.

--- main.py
+++ main.py
@@ -12,21 +12,22 @@ batch = data.test.next_batch(50)
 # webapp
 app = Flask(__name__)
 
-
+@app.route('/api/mnist', methods=['POST'])
 def mnist():
     input = batch[0] 
-    #im = Image.fromarray((batch[0].reshape(28,28)*255).astype(np.uint8))
-    #im.save("/mnist-serving/ckpt/input.jpeg")
+    im = Image.fromarray((batch[0][0].reshape(28,28)*255).astype(np.uint8))
+    im.save("/mnist-serving/ckpt/input.jpeg")
     #print(np.array(request.json))
-    #webIm = Image.fromarray(255 - np.array(request.json).astype(np.uint8).reshape(28,28))
-    #webIm.save("/mnist-serving/ckpt/webIm.jpeg")
+    webIm = Image.fromarray(255 - np.array(request.json).astype(np.uint8).reshape(28,28))
+    webIm.save("/mnist-serving/ckpt/webIm.jpeg")
 
     print(np.argmax(batch[1],axis=1))
     # output1 = regression(input)
+    output = inference(input)
     output2 = np.array(inference(input)).reshape(50,10)
     #print(output2)
     print(np.argmax(output2, axis = 1))
-
+    return jsonify(results=[output, output])    
 
 
 @app.route('/')
@@ -35,4 +36,4 @@ def main():
 
 
 if __name__ == '__main__':
-    mnist()
+    app.run(host='0.0.0.0', port=9005)
