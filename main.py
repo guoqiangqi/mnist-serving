3f366a48e0350403ca5c6a2fb9d62e49fc5502e2
Guoqiang QI <guoqiang.qi1@gmail.com>
2022-01-26 09:02:48 +0000

    Save input images for debug.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
 import tensorflow as tf
 from flask import Flask, jsonify, render_template, request
+from PIL import Image
 
 from mnist_inference import inference
+from tensorflow.examples.tutorials.mnist import input_data
+
+data = input_data.read_data_sets("/mnist-serving/test_data", one_hot=True)
+batch = data.test.next_batch(1)
 
 # webapp
 app = Flask(__name__)
@@ -10,7 +15,14 @@ app = Flask(__name__)
 
 @app.route('/api/mnist', methods=['POST'])
 def mnist():
-    input = (np.array(request.json, dtype=np.uint8) / 255.0).reshape(1, 784)
+    input = batch[0] 
+    im = Image.fromarray((batch[0].reshape(28,28)*255).astype(np.uint8))
+    im.save("/mnist-serving/ckpt/input.jpeg")
+    #print(np.array(request.json))
+    webIm = Image.fromarray(255 - np.array(request.json).astype(np.uint8).reshape(28,28))
+    webIm.save("/mnist-serving/ckpt/webIm.jpeg")
+
+    print(batch[1])
     # output1 = regression(input)
     output2 = inference(input)
     return jsonify(results=[output2, output2])
@@ -22,4 +34,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9003)
+    app.run(host='0.0.0.0', port=9005)
