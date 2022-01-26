7bdb4f89fe826da62c2954b846b579b0f732b06d
Guoqiang QI <guoqiang.qi1@gmail.com>
2022-01-26 10:06:49 +0000

    Output labels for debug.

--- main.py
+++ main.py
@@ -7,25 +7,26 @@ from mnist_inference import inference
 from tensorflow.examples.tutorials.mnist import input_data
 
 data = input_data.read_data_sets("/mnist-serving/test_data", one_hot=True)
-batch = data.test.next_batch(1)
+batch = data.test.next_batch(50)
 
 # webapp
 app = Flask(__name__)
 
 
-@app.route('/api/mnist', methods=['POST'])
 def mnist():
     input = batch[0] 
-    im = Image.fromarray((batch[0].reshape(28,28)*255).astype(np.uint8))
-    im.save("/mnist-serving/ckpt/input.jpeg")
+    #im = Image.fromarray((batch[0].reshape(28,28)*255).astype(np.uint8))
+    #im.save("/mnist-serving/ckpt/input.jpeg")
     #print(np.array(request.json))
-    webIm = Image.fromarray(255 - np.array(request.json).astype(np.uint8).reshape(28,28))
-    webIm.save("/mnist-serving/ckpt/webIm.jpeg")
+    #webIm = Image.fromarray(255 - np.array(request.json).astype(np.uint8).reshape(28,28))
+    #webIm.save("/mnist-serving/ckpt/webIm.jpeg")
 
-    print(batch[1])
+    print(np.argmax(batch[1],axis=1))
     # output1 = regression(input)
-    output2 = inference(input)
-    return jsonify(results=[output2, output2])
+    output2 = np.array(inference(input)).reshape(50,10)
+    #print(output2)
+    print(np.argmax(output2, axis = 1))
+
 
 
 @app.route('/')
@@ -34,4 +35,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9005)
+    mnist()
