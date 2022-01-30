73a404d3caba25ebf6d2fe043b511607429f641a
Guoqiang QI <guoqiang.qi1@gmail.com>
2022-01-30 15:54:25 +0800

    Remove extral blank line.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from mnist_inference import inference
 # webapp
 app = Flask(__name__)
 
-
 @app.route('/api/mnist', methods=['POST'])
 def mnist():
     input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
@@ -15,11 +14,9 @@ def mnist():
     output2 = inference(input)
     return jsonify(results=[output2, output2])
 
-
 @app.route('/')
 def main():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9003)
